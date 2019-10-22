8ccf65c06402940eaacf6a15afa3d567023f0175
Shama Hoque <shama.hoque@gmail.com>
2019-10-22 14:27:08 +0600

    update Hello World to function

--- client/HelloWorld.js
+++ client/HelloWorld.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { hot } from 'react-hot-loader'
 
-class HelloWorld extends Component {
-  render() {
+const HelloWorld = () => {
     return (
         <div>
           <h1>Hello World!</h1>
         </div>
       )
-  }
 }
 
 export default hot(module)(HelloWorld)
